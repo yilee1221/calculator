21733067db3651a888acd782b97a45a62b7bd05e
Erin Lee <123915538+yilee1221@users.noreply.github.com>
2023-05-24 15:54:47 +0900

    add hover effect, better functionality

--- script.js
+++ script.js
@@ -28,26 +28,33 @@ function operate(operator,a,b) {
 }
 
 let display = document.querySelector('.display');
-display.textContent = '';
-let numA;
-let numB;
+let numA = '';
+let numB = '';
 let choseOperator = false;
 let operator = '';
 let answer = 0;
 
+function roundAnswer(number) {
+    return Math.round(number*1000) / 1000;
+}
+
 const numButtons = document.querySelectorAll('.numbers');
 numButtons.forEach((number) => number.addEventListener('click', () => {
     if (!choseOperator) {
+        if (display.textContent == '0') {
+            display.textContent = '';
+        }
         display.textContent += number.textContent;
         numA = parseInt(display.textContent);
         console.log('numA' + numA);
     }
     else {
+        if (numB == '') {
+            display.textContent = '';
+        }
         display.textContent += number.textContent;
         numB = parseInt(display.textContent);
         console.log('numB' + numB)
-        //display.textContent = '';
-        //choseOperator = false;
     }
 }));
 
@@ -55,12 +62,11 @@ const operators = document.querySelectorAll('.operators');
 operators.forEach((operation) => operation.addEventListener('click', () => {
     operator = operation.textContent
     choseOperator = true;
-    display.textContent = '';
 }));
 
 const equals = document.querySelector(".equal");
 equals.addEventListener('click', () => {
-    answer = operate(operator,numA,numB);
+    answer = roundAnswer(operate(operator,numA,numB));
     display.textContent = answer;
     console.log('answer' + answer)
     numA = answer;
@@ -70,7 +76,7 @@ const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
     numA = 0;
     numB = 0;
-    display.textContent = '';
+    display.textContent = 0;
     operator = ''
     choseOperator = false;
 });
